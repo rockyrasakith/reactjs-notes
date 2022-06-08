4491c520fee6725da366740bf83be1029587506b
Rocky Rasakith <rocky.rasakith@saile.ai>
2022-06-08 14:53:46 +0200

    finished code

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -37,17 +37,28 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Update the feedback
-  const updateFeedback = (id, updatedItem) => {
+  const updateFeedback = async (id, updatedItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      }, body: JSON.stringify(updatedItem)
+    });
+
+    const data = await response.json()
+
     setFeedback(
       feedback.map((item) =>
-        item.id === id ? { ...item, ...updatedItem } : item
+        item.id === id ? { ...item, ...data } : item
       )
     );
   };
 
   //This code deletes the feedback after a confirmation window.
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      //Fetch method of a DELETE request
+      await fetch(`/feedback/${id}`, { method: "DELETE" });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
